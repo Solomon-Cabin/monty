197432dfeb3329a08763aff39e6f34a60145e477
Solomon-Cabin <nnamdis64@gmail.com>
2023-12-15 18:31:45 -0800

    project updated

---
+++ README.md
@@ -0,0 +1,1 @@
+a readme file for stacks, queues - LIFO - FIFO
---
+++ stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * f_stack - this function prints the top
+ * @head: the stack head
+ * @counter: line_number
+ * Return: no return
+*/
+
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
